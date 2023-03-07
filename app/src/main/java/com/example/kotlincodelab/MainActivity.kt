ffc73461fdd4ad996f32c0bace3cac6e10ff697d
na0 <skdud907@naver.com>
2023-03-07 14:38:00 +0900

    init commit

---
+++ app/src/main/java/com/example/kotlincodelab/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlincodelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
