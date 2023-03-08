01c2592eff96c6f39c3fcdec465d0b7fc43e1507
na0 <skdud907@naver.com>
2023-03-09 00:34:59 +0900

    add title fragment - AndroidTriviaFragment 앱에서 디자인 가져옴

---
+++ app/src/main/java/com/example/kotlincodelab/GameWonFragment.kt
@@ -0,0 +1,4 @@
+package com.example.kotlincodelab
+
+class GameWonFragment {
+}
--- app/src/main/java/com/example/kotlincodelab/MainActivity.kt
+++ app/src/main/java/com/example/kotlincodelab/MainActivity.kt
@@ -13,27 +13,14 @@ import com.example.kotlincodelab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val myName: MyName = MyName("Na0's")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.myName = myName
-        binding.doneButton.setOnClickListener {
-            addNickname(it)
-        }
+
     }
 
     private fun addNickname(view: View) {
-        binding.apply {
-            myName?.nickname = nicknameEdit.text.toString()
-            nicknameEdit.visibility = View.GONE
-            doneButton.visibility = View.GONE
-            nicknameText.visibility = View.VISIBLE
-            invalidateAll()
-        }
 
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
