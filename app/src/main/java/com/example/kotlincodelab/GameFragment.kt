fe8060b21d4a41ccf2b4bd98983e0ef8413bca62
na0 <skdud907@naver.com>
2023-03-10 16:33:07 +0900

    add GameOverFragment.kt, GameWonFragment.kt - add fragment_game_over.xml, fragment_game_won.xml, try_again.xml, you_win.xml - popUpTo action - backpressed action

--- app/src/main/java/com/example/kotlincodelab/GameFragment.kt
+++ app/src/main/java/com/example/kotlincodelab/GameFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.kotlincodelab.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
@@ -13,6 +14,27 @@ class GameFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentGameBinding>(inflater,
             R.layout.fragment_game, container, false)
+
+        binding.submitButton.setOnClickListener @Suppress("UNUSED_ANONYMOUS_PARAMETER"){
+            val checkedId = binding.questionRadioGroup.checkedRadioButtonId
+
+            if(-1 != checkedId) {
+                var answerIndex = 0
+                when(checkedId) {
+                    R.id.firstAnswerRadioButton -> answerIndex = 1
+                    R.id.secondAnswerRadioButton -> answerIndex = 2
+                    R.id.thirdAnswerRadioButton -> answerIndex = 3
+                    R.id.fourthAnswerRadioButton -> answerIndex = 4
+                }
+                if(answerIndex == 2) {
+                    //won
+                    it.findNavController().navigate(R.id.action_gameFragment_to_gameWonFragment)
+                } else {
+                    //over
+                    it.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment)
+                }
+            }
+        }
         return binding.root
     }
 }
--- app/src/main/java/com/example/kotlincodelab/GameOverFragment.kt
+++ app/src/main/java/com/example/kotlincodelab/GameOverFragment.kt
@@ -1,4 +1,22 @@
 package com.example.kotlincodelab
 
-class GameOverFragment {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.kotlincodelab.databinding.FragmentGameOverBinding
+
+class GameOverFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentGameOverBinding>(inflater,
+            R.layout.fragment_game_over, container, false)
+
+        binding.tryAgainButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_gameOverFragment_to_gameFragment)
+        }
+        return binding.root
+    }
 }
