3ae6af9c459fbcc8bef6266ea80c8a5250a6bc98
na0 <skdud907@naver.com>
2023-03-10 17:28:36 +0900

    add AboutFragment.kt, RulesFragment.kt, fragment_about.xml, fragment_rules.xml - AppBar, DrawLayout 생성 및 NavController와 연결

--- app/src/main/java/com/example/kotlincodelab/MainActivity.kt
+++ app/src/main/java/com/example/kotlincodelab/MainActivity.kt
@@ -4,13 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.kotlincodelab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+
+        drawerLayout = binding.drawerLayout
+
+        //NavController와 DrawerLayout 연결
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        //NavController와 AppBar 연결1
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    }
+
+    //NavController와 AppBar, DrawLayout 연결2
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
--- app/src/main/java/com/example/kotlincodelab/TitleFragment.kt
+++ app/src/main/java/com/example/kotlincodelab/TitleFragment.kt
@@ -1,12 +1,11 @@
 package com.example.kotlincodelab
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.kotlincodelab.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -18,6 +17,18 @@ class TitleFragment : Fragment() {
             it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
 
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item, requireView().findNavController()
+        ) || super.onOptionsItemSelected(item)
     }
 }
